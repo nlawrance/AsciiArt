24867a0d8d1b522bd1406db9aac3be017e1deb92
nlawrance <lawrancenicholas@gmail.com>
2022-12-04 11:46:02 +0800

    It compiles now (y)

--- main.cpp
+++ main.cpp
@@ -7,77 +7,9 @@
 #include "PixelList.h"
 
 #include <iostream>
+#include <set>
 #include <string.h>
 #include <stdio.h>
-
-int     opterr = 1,             /* if error message should be printed */
-        optind = 1,             /* index into parent argv vector */
-        optopt,                 /* character checked for validity */
-        optreset;               /* reset getopt */
-char    *optarg;                /* argument associated with option */
-
-#define BADCH   (int)'?'
-#define BADARG  (int)':'
-#define EMSG    ""
-
-/*
- * getopt --
- *      Parse argc/argv argument vector.
- */
-int getopt(int nargc, char * const nargv[], const char *ostr)
-{
-  static char *place = EMSG;              /* option letter processing */
-  const char *oli;                              /* option letter list index */
-
-  if (optreset || !*place) {              /* update scanning pointer */
-    optreset = 0;
-    if (optind >= nargc || *(place = nargv[optind]) != '-') {
-      place = EMSG;
-      return (-1);
-    }
-    if (place[1] && *++place == '-') {      /* found "--" */
-      ++optind;
-      place = EMSG;
-      return (-1);
-    }
-  }                                       /* option letter okay? */
-  if ((optopt = (int)*place++) == (int)':' ||
-    !(oli = strchr(ostr, optopt))) {
-      /*
-      * if the user didn't specify '-' as an option,
-      * assume it means -1.
-      */
-      if (optopt == (int)'-')
-        return (-1);
-      if (!*place)
-        ++optind;
-      if (opterr && *ostr != ':')
-        (void)printf("illegal option -- %c\n", optopt);
-      return (BADCH);
-  }
-  if (*++oli != ':') {                    /* don't need argument */
-    optarg = NULL;
-    if (!*place)
-      ++optind;
-  }
-  else {                                  /* need an argument */
-    if (*place)                     /* no white space */
-      optarg = place;
-    else if (nargc <= ++optind) {   /* no arg */
-      place = EMSG;
-      if (*ostr == ':')
-        return (BADARG);
-      if (opterr)
-        (void)printf("option requires an argument -- %c\n", optopt);
-      return (BADCH);
-    }
-    else                            /* white space */
-      optarg = nargv[optind];
-    place = EMSG;
-    ++optind;
-  }
-  return (optopt);                        /* dump back option letter */
-}
 
 int main(int argc, char** argv)
 {
@@ -96,55 +28,78 @@ int main(int argc, char** argv)
     bool info = false;
     std::string mosaicPixelDirectory = "";
 
-    int opt;
-    opterr = 0;
-    while ((opt = getopt(argc, argv, OPTLIST)) != -1) {
-        switch (opt)
+    // Ignore the program name.
+    argc--;
+    argv++;
+
+    // Read the optional arguments.
+    while (argc > 0)
+    {
+        if ((*argv)[0] != '-')
         {
-            case 'a':
-                getAverageColour = true;
-                break;
-            case 'd':
-                isDebug = true;
-                break;
-            case 'f':
-                createPixelListFile = true;
-                break;
-            case 'i':
-                info = true;
-                break;
-            case 'm':
-                mosaicPixelDirectory = strdup(optarg);
-                break;
-            default:
-                throw std::invalid_argument("Invalid option.");
+            // Found all the optional arguments.
+            break;
         }
-    }
-    
-    argc -= optind;
-    argv += optind;       
+        else
+        {
+            bool skipNext = false;
+            for (int i = 1; (*argv)[i] != '\0'; i++)
+            {
+                switch ((*argv)[i])
+                {
+                    case 'a':
+                        getAverageColour = true;
+                        break;
+                    case 'd':
+                        isDebug = true;
+                        break;
+                    case 'f':
+                        createPixelListFile = true;
+                        break;
+                    case 'i':
+                        info = true;
+                        break;
+                    case 'm':
+                        mosaicPixelDirectory = strdup(*(argv + 1));
+                        skipNext = true;
+                        break;
+                    default:
+                        throw std::invalid_argument("Invalid option.");
+                }
+                if (skipNext)
+                {
+                    argc--;
+                    argv++;
+                }
+            }
+        }
+
+        // Move to the next argument.
+        argc--;
+        argv++;
+    }      
     
 
     if (createPixelListFile)
     {
         PixelList list = PixelList();
         list.CreatePixelImageListFile(mosaicPixelDirectory);
-    exit(EXIT_SUCCESS);
+        exit(EXIT_SUCCESS);
     }
 
-  if (argc <= 0) {
-    std::cerr << helpText << std::endl;
-    exit(EXIT_FAILURE);
-  }
+    if (argc <= 0) {
+        std::cerr << helpText << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
     std::string filename = *argv; 
   
-  BitmapImage image = BitmapImage(filename);
+    BitmapImage image = BitmapImage(filename);
     if (isDebug)
     {
         std::cout << image.HeaderString();
     }
-  image.ReadPixelMarix();
+    image.ReadPixelMarix();
     if (getAverageColour)
     {
         std::cout << static_cast<int>(image.GetAverageGreyScaleValue()) << "_\n";
