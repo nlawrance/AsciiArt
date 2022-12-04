58d3678ac8d67b68ab90580b95e58681d546364e
nlawrance <lawrancenicholas@gmail.com>
2022-12-04 11:22:36 +0800

    Tabs to spaces

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /**
-*	Created by Nicholas Lawrance 10th June 2018
+*    Created by Nicholas Lawrance 10th June 2018
 */
 
 #include "BitmapImage.h"
@@ -79,88 +79,89 @@ int getopt(int nargc, char * const nargv[], const char *ostr)
   return (optopt);                        /* dump back option letter */
 }
 
-int main( int argc, char** argv ) {
-	const char*	OPTLIST	= "adfim:";
-	const std::string helpText = 
-		"./AsciiArt [-adi] [-m pDir] filename\n"
-		"\t-a\t\tReturns the average colour of the given file\n"
-		"\t-d\t\tAcivate debug logging\n"
-		"\t-f\t\tGenerate pixel list file\n"
-		"\t-i\t\tActivate info mode (disable creation of mosaic image)\n"
-		"\t-m\tpDir\tDirectory to get pixel images from";
+int main(int argc, char** argv)
+{
+    const char* OPTLIST = "adfim:";
+    const std::string helpText = 
+        "./AsciiArt [-adfi] [-m pDir] filename\n"
+        "\t-a\t\tReturns the average colour of the given file\n"
+        "\t-d\t\tAcivate debug logging\n"
+        "\t-f\t\tGenerate pixel list file\n"
+        "\t-i\t\tActivate info mode (disable creation of mosaic image)\n"
+        "\t-m\tpDir\tDirectory to get pixel images from";
 
-	bool getAverageColour = false;
-	bool createPixelListFile = false;
-	bool isDebug = false;
-	bool info = false;
-	std::string mosaicPixelDirectory = "";
-	
-	int opt;
-  opterr = 0;
-	while ((opt = getopt(argc, argv, OPTLIST)) != -1) {
-		switch (opt)
-		{
-			case 'a':
-				getAverageColour = true;
-				break;
-			case 'd':
-				isDebug = true;
-				break;
-			case 'f':
-				createPixelListFile = true;
-				break;
-			case 'i':
-				info = true;
-				break;
-			case 'm':
-				mosaicPixelDirectory = strdup(optarg);
-				break;
-			default:
-				throw std::invalid_argument("Invalid option.");
-		}
-	}
-	
-	argc -= optind;
-	argv += optind;       
+    bool getAverageColour = false;
+    bool createPixelListFile = false;
+    bool isDebug = false;
+    bool info = false;
+    std::string mosaicPixelDirectory = "";
+
+    int opt;
+    opterr = 0;
+    while ((opt = getopt(argc, argv, OPTLIST)) != -1) {
+        switch (opt)
+        {
+            case 'a':
+                getAverageColour = true;
+                break;
+            case 'd':
+                isDebug = true;
+                break;
+            case 'f':
+                createPixelListFile = true;
+                break;
+            case 'i':
+                info = true;
+                break;
+            case 'm':
+                mosaicPixelDirectory = strdup(optarg);
+                break;
+            default:
+                throw std::invalid_argument("Invalid option.");
+        }
+    }
+    
+    argc -= optind;
+    argv += optind;       
     
 
-	if (createPixelListFile)
-	{
-		PixelList list = PixelList();
-		list.CreatePixelImageListFile(mosaicPixelDirectory);
+    if (createPixelListFile)
+    {
+        PixelList list = PixelList();
+        list.CreatePixelImageListFile(mosaicPixelDirectory);
     exit(EXIT_SUCCESS);
-	}
+    }
 
   if (argc <= 0) {
     std::cerr << helpText << std::endl;
     exit(EXIT_FAILURE);
   }
 
-	std::string filename = *argv; 
+    std::string filename = *argv; 
   
   BitmapImage image = BitmapImage(filename);
-	if (isDebug)
-	{
-		std::cout << image.HeaderString();
-	}
+    if (isDebug)
+    {
+        std::cout << image.HeaderString();
+    }
   image.ReadPixelMarix();
-	if (getAverageColour)
-	{
-		std::cout << static_cast<int>(image.GetAverageGreyScaleValue()) << "_\n";
-	}
+    if (getAverageColour)
+    {
+        std::cout << static_cast<int>(image.GetAverageGreyScaleValue()) << "_\n";
+    }
   
-	if (!info)
-	{
-		if (mosaicPixelDirectory.empty())
-		{
-			std::cout << image.PrintASCIIPixelMatrix(true);
-		}
-		else
-		{
-			MosaicImage mosaicImage(mosaicPixelDirectory);
-			mosaicImage.CreateMosaicImage(image);
-		}
-	}
+    if (!info)
+    {
+        if (mosaicPixelDirectory.empty())
+        {
+            std::cout << image.PrintASCIIPixelMatrix(true);
+        }
+        else
+        {
+            MosaicImage mosaicImage(mosaicPixelDirectory);
+            mosaicImage.CreateMosaicImage(image);
+        }
+    }
   
-	return 0;
+    return 0;
 } 
